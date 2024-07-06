566e7abd22c2c25047835fd358538380a5d57145
Zylo23 <pyyupsk@proton.me>
2024-07-06 14:52:12 +0700

    fix: adjust BASE_URL in Layout component and site URL in astro.config.mjs
    - Update astro.config.mjs to correct site URL to 'https://safecy.github.io'.
    - Modify BASE_URL in Layout.astro to append '/password-generator'.
    
    This commit fixes URL configurations to ensure correct linking and integration.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://safecy.github.io/password-generator',
+	site: 'https://safecy.github.io',
 	output: 'static',
 	integrations: [
 		tailwind({
